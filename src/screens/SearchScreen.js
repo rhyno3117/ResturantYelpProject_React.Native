cfb56465d6d709cd6406960e118f50470eead0f7
ryan <rhynogolfer42@gmail.com>
2024-04-22 14:57:39 -0700

    added SearchScreen.js in src/screens folder

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>SearchScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default SearchScreen
