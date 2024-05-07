838cdcc5a4991d333cfdc28dbb3e67c5fd99437e
ryan <rhynogolfer42@gmail.com>
2024-05-07 14:23:18 -0700

    added and imported Feather Search Icon

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { Feather } from '@expo/vector-icons'
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
+        <Feather name="search" />
       <Text>Search Bar</Text>
     </View>
   )
