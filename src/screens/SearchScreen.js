e42513982d6b0fe6ce1ff610e8cf9b7bcc5f3d5d
ryan <rhynogolfer42@gmail.com>
2024-05-07 14:50:23 -0700

    added and created a useState in SearchScreen and having it be passed down to SearchBar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState }from 'react'
 import SearchBar from '../components/SearchBar'
 
 const SearchScreen = () => {
+const [term, setTerm] = useState('')
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} />
       <Text>SearchScreen</Text>
     </View>
   )
