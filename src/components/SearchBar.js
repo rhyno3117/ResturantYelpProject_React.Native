e5e1c7e9b3828be67f241ac4184541286cd23884
ryan <rhynogolfer42@gmail.com>
2024-05-07 13:53:22 -0700

    SearchBar.js component added in components folder in src folder with boiler plate RFCE code

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>SearchScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+})
+
+export default SearchScreen
