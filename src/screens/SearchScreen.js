dc70e0d4b041e3730a573e2a3d1abb4bb818cc03
ryan <rhynogolfer42@gmail.com>
2024-05-07 14:58:57 -0700

    all state is now managed through SearchScreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,7 +7,11 @@ const [term, setTerm] = useState('')
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} />
+      <SearchBar 
+      term={term} 
+      onTermChange={(newTerm) => setTerm(newTerm)} 
+      onTermSubmit={() => console.log('term was submitted')}
+      />
       <Text>SearchScreen</Text>
       <Text>{term}</Text>
     </View>
