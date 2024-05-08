50da927727340c49aa1a170138189d14567d1d10
ryan <rhynogolfer42@gmail.com>
2024-05-08 11:46:25 -0700

    update

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React, { useState }from 'react'
+import React, { useState } from 'react'
 import SearchBar from '../components/SearchBar'
 
 const SearchScreen = () => {
