8d7002d203ca59fda1ee1d4e964ef009d8e104a1
ryan <rhynogolfer42@gmail.com>
2024-05-09 10:09:27 -0700

    created yelp API setup to import into files in future

---
+++ src/api/yelp.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: 'Bearer D9Wp-pYZ28RFGoeGz8vLdw-kUQA6gF7Mke6WNJAXL1YCe4K1yCdB31hkKVjClktwFUyj9N-g1ECuIY0jHYMF-rRf_rJYtl6jMjePpJ9iJOJougVUUUOxlXtSIQojZnYx' 
+    }
+});
