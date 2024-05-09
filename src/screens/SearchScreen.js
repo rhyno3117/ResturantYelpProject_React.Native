0d2701a6bbf47a5697fd862db49c4840b2eb9f80
ryan <rhynogolfer42@gmail.com>
2024-05-09 10:19:35 -0700

    added params that any key value pairs will get added to request url

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,15 +4,24 @@ import SearchBar from '../components/SearchBar'
 import yelp from '../api/yelp'
 
 const SearchScreen = () => {
-const [term, setTerm] = useState('');
-const [results, setResults] = useState([]);
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        
+      }
+    });
+    setResults(response.data.businesses);
+  };
 
   return (
     <View>
-      <SearchBar 
-      term={term} 
-      onTermChange={(newTerm) => setTerm(newTerm)} 
-      onTermSubmit={() => console.log('term was submitted')}
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => console.log('term was submitted')}
       />
       <Text>SearchScreen</Text>
       <Text>We have found {results.length} results</Text>
