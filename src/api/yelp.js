dcb1dabb9ac994c1c595c48dc5f38e54138b7504
ryan <rhynogolfer42@gmail.com>
2024-05-10 14:12:55 -0700

    updated yelp.js with space

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'BearerD9Wp-pYZ28RFGoeGz8vLdw-kUQA6gF7Mke6WNJAXL1YCe4K1yCdB31hkKVjClktwFUyj9N-g1ECuIY0jHYMF-rRf_rJYtl6jMjePpJ9iJOJougVUUUOxlXtSIQojZnYx' 
+        Authorization: 'Bearer D9Wp-pYZ28RFGoeGz8vLdw-kUQA6gF7Mke6WNJAXL1YCe4K1yCdB31hkKVjClktwFUyj9N-g1ECuIY0jHYMF-rRf_rJYtl6jMjePpJ9iJOJougVUUUOxlXtSIQojZnYx' 
     }
 });
