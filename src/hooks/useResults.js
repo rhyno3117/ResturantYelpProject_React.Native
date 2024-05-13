0b89503d0f0d90d306288358f3f153fe4db20fe5
ryan <rhynogolfer42@gmail.com>
2024-05-13 11:44:24 -0700

    copied the search api code from SearchScreen over to useResults.js file

---
+++ src/hooks/useResults.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import yelp from '../api/yelp'
+
+export default () => {
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (searchTerm) => {
+        console.log('Hi There');
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
+    };
+
+    useEffect(() => {
+        searchApi('pasta')
+    }, []);
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,28 +5,6 @@ import yelp from '../api/yelp'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async (searchTerm) => {
-    console.log('Hi There');
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose'
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
-
-  useEffect(() => {
-    searchApi('pasta')
-  }, []);
 
   return (
     <View>
