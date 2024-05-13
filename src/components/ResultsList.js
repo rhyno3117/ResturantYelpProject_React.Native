bd8107c6cf2c4b306b85fccc0d2ede4778e580e4
ryan <rhynogolfer42@gmail.com>
2024-05-13 11:53:20 -0700

    added ResultsList component with starter imports and code

---
+++ src/components/ResultsList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+
+function ResultsList() {
+  return (
+    <View>
+        <Text>ResultsList</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default ResultsList
