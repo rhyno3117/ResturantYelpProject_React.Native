c7ac45c7063cee1e3e33f3b9a39a7807108bbef9
ryan <rhynogolfer42@gmail.com>
2024-05-15 11:55:16 -0700

    image is now appearing on application:

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -4,11 +4,18 @@ import { View, Image, Text, StyleSheet } from 'react-native';
 const ResultsDetail = ({ result }) => {
   return (
     <View>
+        <Image style={styles.image} source={{ uri: result.image_url }} />
         <Text>{result.name}</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: 250,
+        height: 120,
+        borderRadius: 4
+    }
+});
 
 export default ResultsDetail;
