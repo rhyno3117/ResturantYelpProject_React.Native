3355af6e844db0e40b9d71e22fb2655529c650d2
ryan <rhynogolfer42@gmail.com>
2024-05-14 22:45:59 -0700

    created another component as helper to show individual result for each

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Image, Text, StyleSheet } from 'react-native';
+
+function ResultsDetail() {
+  return (
+    <View>
+        <Text>Results detail</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default ResultsDetail;
