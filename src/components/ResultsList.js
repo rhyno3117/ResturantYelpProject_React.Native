c2afff05e1b4967fbaad67edf1173279c1279e36
ryan <rhynogolfer42@gmail.com>
2024-05-19 11:44:17 -0700

    passed down prop of navigation down to ResultsList and also imported TouchableOpacity

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import ResultsDetail from './ResultsDetail';
 
-function ResultsList({ title, results }) {
+function ResultsList({ title, results, navigation }) {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
