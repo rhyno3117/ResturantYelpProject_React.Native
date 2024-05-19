bdb2bc224f4eae9357f57e5ec517c08655a75fec
ryan <rhynogolfer42@gmail.com>
2024-05-19 11:55:37 -0700

    took out all mentions of navigation from SearchScreen and then in ResultsList I imported withNavigation from react-navigation and in process of doing this way

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import ResultsDetail from './ResultsDetail';
+import { withNavigation } from 'react-navigation';
 
 function ResultsList({ title, results, navigation }) {
     return (
