098e76d03f269536f51286bd81a5190dc39b8efd
ryan <rhynogolfer42@gmail.com>
2024-05-20 11:49:44 -0700

    added if statement to show null

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -6,8 +6,6 @@ const ResultsShowScreen = ({ route }) => {
   const [result, setResult] = useState(null);
   const { id } = route.params;
 
-  console.log(result)
-
   const getResult = async (id) => {
     const response = await yelp.get(`/${id}`);
     setResult(response.data);
@@ -16,6 +14,10 @@ const ResultsShowScreen = ({ route }) => {
   useEffect(() => {
     getResult(id);
   }, []);
+
+  if (!result) {
+    return null;
+  }
 
   return (
     <View>
